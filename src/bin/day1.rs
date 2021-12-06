1a695b4236fb97057cb1d24b3d36506b16ecd389
Paweł Dąbrowski <dabrowskip9@gmail.com>
2021-12-06 20:44:06 +0100

    add rustfmt config

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -2,49 +2,50 @@ use aoc2021::get_input;
 use std::io::*;
 
 fn read_input() -> Vec<u16> {
-    get_input(1)
-        .lines()
-        .map(|line| {
-            line.expect("could not read line")
-                .parse::<u16>()
-                .expect("could not parse string")
-        })
-        .collect()
+  get_input(1)
+    .lines()
+    .map(|line| {
+      line
+        .expect("could not read line")
+        .parse::<u16>()
+        .expect("could not parse string")
+    })
+    .collect()
 }
 
 fn part_1(input: &Vec<u16>) -> u16 {
-    let mut increases: u16 = 0;
+  let mut increases: u16 = 0;
 
-    for (index, value) in input.iter().enumerate() {
-        if index > 0 {
-            let prev_value = input[index - 1];
-            if value > &prev_value {
-                increases += 1;
-            }
-        }
+  for (index, value) in input.iter().enumerate() {
+    if index > 0 {
+      let prev_value = input[index - 1];
+      if value > &prev_value {
+        increases += 1;
+      }
     }
+  }
 
-    increases
+  increases
 }
 
 fn part_2(input: &Vec<u16>) -> u16 {
-    let mut increases: u16 = 0;
+  let mut increases: u16 = 0;
 
-    // far from optimal
-    for i in 0..(input.len() - 3) {
-        let window_1_sum: u16 = input[i..i + 3].iter().sum();
-        let window_2_sum: u16 = input[i + 1..i + 4].iter().sum();
+  // far from optimal
+  for i in 0..(input.len() - 3) {
+    let window_1_sum: u16 = input[i..i + 3].iter().sum();
+    let window_2_sum: u16 = input[i + 1..i + 4].iter().sum();
 
-        if window_2_sum > window_1_sum {
-            increases += 1
-        }
+    if window_2_sum > window_1_sum {
+      increases += 1
     }
+  }
 
-    increases
+  increases
 }
 
 fn main() {
-    let data = read_input();
-    println!("Part 1: {}", part_1(&data));
-    println!("Part 2: {}", part_2(&data));
+  let data = read_input();
+  println!("Part 1: {}", part_1(&data));
+  println!("Part 2: {}", part_2(&data));
 }
