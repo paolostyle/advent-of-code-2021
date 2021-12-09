71111f8d982ddb44f8d3a2ac886534e3bd3d5b56
Paweł Dąbrowski <dabrowskip9@gmail.com>
2021-12-09 03:14:20 +0100

    simplify day 7 solution a little

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,5 +1,5 @@
 use aoc2021::get_input;
-use std::{collections::HashMap, io::BufRead, time::Instant};
+use std::{io::BufRead, time::Instant};
 
 fn read_input() -> Vec<i32> {
   get_input(7)
@@ -16,29 +16,26 @@ fn read_input() -> Vec<i32> {
 
 fn part_1(input: &Vec<i32>) -> i32 {
   let max_val = *input.iter().max().unwrap();
-  let mut results = HashMap::new();
+  let mut min_result = i32::MAX;
 
+  // this is kinda brute force'y, prob there are better solutions
   for end_pos in 0..max_val {
     let mut total_fuel_used = 0;
     for crab_pos in input {
       let fuel_used = (crab_pos - end_pos).abs();
       total_fuel_used += fuel_used;
     }
-    results.insert(end_pos, total_fuel_used);
+    if total_fuel_used < min_result {
+      min_result = total_fuel_used;
+    }
   }
 
-  let key = results
-    .iter()
-    .min_by(|a, b| a.1.cmp(&b.1))
-    .map(|(k, _v)| k)
-    .unwrap();
-
-  results[key]
+  min_result
 }
 
 fn part_2(input: &Vec<i32>) -> f64 {
   let max_val = *input.iter().max().unwrap();
-  let mut results = HashMap::new();
+  let mut min_result = f64::INFINITY;
 
   for end_pos in 0..max_val {
     let mut total_fuel_used = 0.0;
@@ -47,16 +44,12 @@ fn part_2(input: &Vec<i32>) -> f64 {
       let fuel_used = (1.0 + distance) / 2.0 * distance;
       total_fuel_used += fuel_used;
     }
-    results.insert(end_pos, total_fuel_used);
+    if total_fuel_used < min_result {
+      min_result = total_fuel_used;
+    }
   }
 
-  let key = results
-    .iter()
-    .min_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
-    .map(|(k, _v)| k)
-    .unwrap();
-
-  results[key]
+  min_result
 }
 
 fn main() {
